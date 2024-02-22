0b21fb55d339e18a4bb662a4f697b1f2a2e8e4c7
Shane T <st83440@gmail.com>
2024-02-22 09:38:59 -0700

    tailwind example, update stylestage instructions

---
+++ modules/examples/nat-parks/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      spacing: {
+        "resp-8": "8vw",
+        "resp-24": "24vw"
+      }
+    }
+  },
+  plugins: []
+};
